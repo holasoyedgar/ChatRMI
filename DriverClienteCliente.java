aef5ddc094f265401c8e16b64343825c931639a1
holasoyedgar <eers222@gmail.com>
2020-06-19 16:17:44 -0500

    Versión final del programa

--- DriverClienteCliente.java
+++ DriverClienteCliente.java
@@ -12,12 +12,13 @@ public class DriverClienteCliente {
 		LocateRegistry.createRegistry(Registry.REGISTRY_PORT);
 		ClienteCliente cliente = new ClienteCliente(name, otroCliente);
 		new Thread(cliente).start();
-	/*	try{
-			System.out.println(java.net.InetAddress.getLocalHost().getHostAddress());
+		try{
+			 Naming.rebind("rmi://" + java.net.InetAddress.getLocalHost().getHostAddress() +":3000/RMIChatClienteCliente", (Remote) cliente);
+	//		System.out.println(java.net.InetAddress.getLocalHost().getHostAddress());
 		}catch(Exception e){
 			e.printStackTrace();
-		}*/
-        Naming.rebind("rmi://192.168.1.74:3000/RMIChatClienteCliente", (Remote) cliente);
+		}
+     //   Naming.rebind("rmi://192.168.1.74:3000/RMIChatClienteCliente", (Remote) cliente);
 	}
     public static void main(String[] args) throws RemoteException, MalformedURLException {
 		//LocateRegistry.createRegistry(Registry.REGISTRY_PORT);
