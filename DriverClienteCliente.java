631065502d483bfc3435b656c6b88dbc77b65b42
holasoyedgar <eers222@gmail.com>
2020-06-18 23:38:10 -0500

    Cliente-Cliente pruebas 2

--- DriverClienteCliente.java
+++ DriverClienteCliente.java
@@ -10,8 +10,9 @@ import java.rmi.registry.Registry;
 public class DriverClienteCliente {
 	public DriverClienteCliente(String name, String otroCliente) throws RemoteException, MalformedURLException{
 		LocateRegistry.createRegistry(Registry.REGISTRY_PORT);
-		new Thread(new ClienteCliente(name, otroCliente)).start();
-        Naming.rebind("RMIChatClienteCliente", (Remote) new ClienteCliente(name, otroCliente));
+		ClienteCliente cliente = new ClienteCliente(name, otroCliente);
+		new Thread(cliente).start();
+        Naming.rebind("RMIChatClienteCliente", (Remote) cliente);
 	}
     public static void main(String[] args) throws RemoteException, MalformedURLException {
 		//LocateRegistry.createRegistry(Registry.REGISTRY_PORT);
